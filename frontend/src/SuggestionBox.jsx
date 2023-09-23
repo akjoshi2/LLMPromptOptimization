98e23a10ea02fd895a44135bbffe84fc5527f1df
akjoshi2 <akjoshi2003@gmail.com>
2023-09-23 14:50:00 -0500

    Fix merge conflict

--- frontend/src/SuggestionBox.jsx
+++ frontend/src/SuggestionBox.jsx
@@ -1,21 +1,8 @@
-import { useEffect, React } from "react";
+import { React } from "react";
 
 function SuggestionBox() {
-<<<<<<< HEAD
-	useEffect(() => {
-        // call api or anything
-	
-
-
-	
-
-});
-
-	
-=======
 	const text = "What is ChatGPT?";
 
->>>>>>> 25020c0838b162261c2cc713055312e0b8260f7b
 	return (
 		<div>
 			{text}
