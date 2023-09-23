9ee905d26b7bd0fc4ef5e1862da497bcab127f40
akjoshi2 <akjoshi2003@gmail.com>
2023-09-23 10:54:34 -0500

    Insert react content into ChatGPT

--- frontend/src/content.js
+++ frontend/src/content.js
@@ -1,3 +1,13 @@
-const reactEle = document.createElement("div");
-reactEle.id = "content-target"
-document.body.insertBefore(reactEle, document.body.firstChild)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import SuggestionBox from "./SuggestionBox.jsx";
+
+const reactElement = document.createElement("div");
+reactElement.id = "content-target";
+const promptElement = document.getElementById("prompt-textarea").parentElement.parentElement;
+const promptContainer = promptElement.parentElement;
+promptContainer.insertBefore(reactElement, promptElement);
+
+const container = document.getElementById("content-target")
+const root = createRoot(container);
+root.render(<SuggestionBox />)
