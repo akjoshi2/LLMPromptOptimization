0b0f155a6660468642d17c5ef708145e9af47f60
Elon <elonmusk@gmail.com>
2023-09-23 14:46:24 -0500

    disable chatgpt to enable extension activities

--- frontend/src/SuggestionBox.jsx
+++ frontend/src/SuggestionBox.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { useEffect, React } from "react";
 
 function SuggestionBox() {
+	useEffect(() => {
+        // call api or anything
+	
+
+
+	
+
+});
+
+	
 	return (
 		<div>
 			My name is Aiden Cohen.
 		</div>
-	)
+
+	);
 }
 
 export default SuggestionBox
--- frontend/src/content.js
+++ frontend/src/content.js
@@ -11,3 +11,31 @@ promptContainer.insertBefore(reactElement, promptElement);
 const container = document.getElementById("content-target")
 const root = createRoot(container);
 root.render(<SuggestionBox />)
+var cool = false;
+const nativeFetch = window.fetch;
+const waitUntil = (condition, res,checkInterval=100) => {
+    return new Promise(resolve => {
+        let interval = setInterval(() => {
+            if (!condition()) return;
+            clearInterval(interval);
+            resolve();
+        }, checkInterval)
+    }).then(() => {
+        cool = false;
+        return nativeFetch.apply(window,res)
+
+    })
+}
+window.fetch = function(...args) {
+
+
+  if(args[0] === "https://chat.openai.com/backend-api/conversation"){
+    
+    return waitUntil(() => cool===true, args)
+
+
+  }
+  return nativeFetch.apply(window,args);
+}
+
+
