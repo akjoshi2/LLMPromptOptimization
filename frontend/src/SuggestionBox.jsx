0c4c2e23e220ac1d0d58a555416b983d58d559d7
Min-Sung Choi <mchoi@wireless-nat-guest-inside.uiowa.edu>
2023-09-23 13:18:32 -0500

    first commit from msc_frontend initial button made

--- frontend/src/SuggestionBox.jsx
+++ frontend/src/SuggestionBox.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './index.css';
 
 function SuggestionBox() {
 	return (
-		<div>
-			My name is Aiden Cohen.
+		<div class="static ...">
+  			<details class="closed:bg-white dark:closed:bg-slate-900 closed:ring-1 closed:ring-black/5 dark:closed:ring-white/10 closed:shadow-lg p-6 rounded-lg" closed>
+    			<summary class="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
+      				Improve your prompt
+    			</summary>
+    			<div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
+    				<p> THIS TEXT BOX SHOULD DISPLAY IMPROVEMENTS THAT CAN BE MADE TO THE PROMPTS </p>
+    			</div>
+  			</details>
 		</div>
 	)
 }
