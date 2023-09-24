28bcd09f1cc7a00938aab7f4c62e42027cf0d6fb
Elon <elonmusk@gmail.com>
2023-09-24 09:55:26 -0500

    Add prompt for code generation type

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ import sqlite3
 
 db = sqlite3.connect("testdb")
 cursor = db.cursor()
-t ="""When translating, try to craft sentences that match the meaning of the original sentences. Do not simply translate every word."""
-cursor.execute("""INSERT INTO categories(name,user) VALUES ('Translation', 'default')""")
-cursor.execute("""INSERT INTO conversation(name,value,cat) VALUES (?,?,?)""", ('default', t, 'Translation'))
+t ="""Hint: Use PrintWriter"""
+cursor.execute("""INSERT INTO conversation(name,value,cat) VALUES (?,?,?)""", ('default', t, 'Code Generation'))
 db.commit()
 db.close()
