d46e74705b5ec87722a67431766e9d386157339d
Elon <elonmusk@gmail.com>
2023-09-24 09:59:27 -0500

    Add default value for other category

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ import sqlite3
 
 db = sqlite3.connect("testdb")
 cursor = db.cursor()
-t ="""Hint: Use PrintWriter"""
-cursor.execute("""INSERT INTO conversation(name,value,cat) VALUES (?,?,?)""", ('default', t, 'Code Generation'))
+t ="""Default"""
+cursor.execute("""INSERT INTO conversation(name,value,cat) VALUES (?,?,?)""", ('default', t, 'other'))
 db.commit()
 db.close()
