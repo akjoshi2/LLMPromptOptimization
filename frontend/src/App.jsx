43a4fc60a96c5ae15a503be259a3ffc3f2823d72
Elon <elonmusk@gmail.com>
2023-09-24 07:13:35 -0500

    hello

---
+++ frontend/src/App.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import React  from "react";
+
+import './index.css'
+import './SuggestionBox.jsx'
+
+function App() {
+
+  const [c, setC] = useState([]);
+  const [text, setText] = useState("");
+  useEffect(() => {
+    fetch('https://uiowa.onrender.com/getcat').then(response => {
+			return response.json()
+		}).then(response => {
+      setC(response.res);
+		})
+
+  });
+
+  var oC = function(){
+    fetch('https://uiowa.onrender.com/get?type=' + encodeURIComponent(input.value)).then(response => {
+			return response.json()
+		}).then(response => {
+      setText(response.value[0][0]);
+		})
+
+  }
+
+  var b = function(){
+
+    let data = FormData();
+    data.append("type" , c)
+    data.append("value", text )
+    fetch('https://uiowa.onrender.com/set', {"method" : "POST", body: data })
+  }
+
+ 
+  return (
+<div class="w-full max-w-xs">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Category Title
+      </label>
+      <input list="s" id="converse" onChange={oC}/>
+      <datalist id="s">
+        {
+         c.map(name => {
+            <option value={name}/>
+
+          })
+        }
+</datalist>   
+
+ </div>
+ <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                Prompt
+      </label>
+      <input id="val" value={text}/>
+
+ </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={b}>
+                  Revise
+      </button>
+    </div>
+  </form>
+</div>
+  );
+}
+
+export default App;
