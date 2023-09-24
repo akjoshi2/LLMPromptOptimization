f212bcd3bd5ef0d9b16c068b82faad1417412326
Elon <elonmusk@gmail.com>
2023-09-23 19:35:49 -0500

    backendv1

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 import requests
+from flask_cors import CORS
 import boto3
 import sqlite3
+import json
+import openai
 
 app = Flask(__name__)
+CORS(app)
 session = boto3.session.Session()
 client = session.client(
     service_name='secretsmanager',
@@ -13,16 +17,39 @@ apikey = client.get_secret_value(
         SecretId="arn:aws:secretsmanager:us-east-2:786987962169:secret:openaiapikey-D5xsWn"
     )
 
-@app.route("/categorize")
+@app.route("/categorize", methods=["POST"])
 def cat_nlp():
-    endpoint = "https://api.openai.com/v1/chat/completions"
-    headers= {"Authorization" : "Bearer " + apikey, "Content-Type": "application/json"}
-    body = {"model" : "gpt-3.5-turbo"}
-    requests.post(endpoint, json=body, headers=headers)
-    return {"answer" : }
+    print(request.form)
+    '''endpoint = "https://api.openai.com/v1/chat/completions"
+    headers= {"Authorization": "Bearer " + json.loads(apikey["SecretString"])["openai"], "Content-Type" : "application/json"}
+    print(headers)
+    messages = [
+      #  {"role" :  "system", "content" : "Your role is to classify the submitted text into one of these categories. Select the category that matches the submitted text. Categories: Math Problem, Essay Generation, Multiple Choice Question"},
+       # {"role" : "user", "content" : "Text: " + request.form["sentence"]}
+       {"role": "user", "content": "What is the OpenAI Mission?"}
+    ]   
+    print(messages)
+    body = {"model" : "gpt-3.5-turbo", "messages" : messages,  "temperature" : .2, "max_tokens": 1024}
+    resp = requests.post(endpoint, json=json.dumps(body), headers=headers)
+    print(resp)
+    print(resp.status_code)
+    return resp.json()["choices"]["message"]'''
+    openai.api_key = json.loads(apikey["SecretString"])["openai"]
+
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Your role is to classify the submitted text into one of these categories. Select the category that matches the submitted text. Categories: "},
+        {"role": "user", "content": "Text: " + request.form["sentence"]}
+    ]
+    )
+    queryType = completion.choices[0].message.content
+    if (queryType == "Math Problem"):
+        return ("Take a deep breath and solve the problem step by step.\n" + request.form["sentence"])
+    #return completion.choices[0].message
 @app.route("/")
 def hello_world():
-    db = sqlite3.connect("/mnt/lambda/testdb")
+    db = sqlite3.connect("testdb")
     create_schema(db)
     return "<p>Hello, World!</p>"
 
@@ -32,3 +59,7 @@ def create_schema(db):
     c.execute(cats)
     c.commit()
     c.close()
+    
+
+if __name__ == "__main__":
+    app.run()
